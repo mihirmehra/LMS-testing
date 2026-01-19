59fe63c55183c921033a128c06a743e6e7026357
mihirmehra <49902630+mihirmehra@users.noreply.github.com>
2026-01-19 09:57:37 +0000

    new updates - stage 2 - 23

--- public/components/theme-provider.tsx
+++ public/components/theme-provider.tsx
@@ -1,11 +1,16 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+interface CustomThemeProviderProps {
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+  children: React.ReactNode
+}
+
+export function ThemeProvider({ children, ...props }: CustomThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
